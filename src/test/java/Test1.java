c4d19052656138cbe288d6d1df980845c6b42c0c
super_kelly <kellycc677@gmail.com>
2021-09-10 07:26:05 +1200

    silent mode

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,14 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class Test1 {
     @Test
     public  void test1(){
         WebDriverManager.chromedriver().setup();
-
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("http://www.google.com");
 
         System.out.println("hello folks");
